37e82c955fec844939efb1d1147eaed8550919b3
claire-russell <russell.claireolivia@gmail.com>
2020-04-06 14:58:30 +1200

    deploy

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: process.env.DATABASE_URL,
       user:     'username',
       password: 'password'
     },
